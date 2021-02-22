2a0d9ef7a7de2fc1fa1475cbdcafa04ffcdc31a8
30james00 <30james00@users.noreply.github.com>
2021-02-23 00:55:51 +0100

    Working layout

--- lib/screens/home_screen/components/home_form.dart
+++ lib/screens/home_screen/components/home_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:paste_law/cubit/quote_cubit.dart';
 import 'package:paste_law/models/quote.dart';
 
 class HomeForm extends StatefulWidget {
@@ -22,79 +24,77 @@ class _HomeFormState extends State<HomeForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Flexible(
-              child: TextFormField(
-                decoration: InputDecoration(labelText: 'Akt prawny'),
-                onSaved: (value) => _quote = Quote(
-                  source: value,
-                  art: _quote.art,
-                  par: _quote.par,
-                  ust: _quote.ust,
-                  pkt: _quote.pkt,
-                ),
+      child: Column(
+        children: [
+          Flexible(
+            child: TextFormField(
+              decoration: InputDecoration(labelText: 'Akt prawny'),
+              onSaved: (value) => _quote = Quote(
+                source: value,
+                art: _quote.art,
+                par: _quote.par,
+                ust: _quote.ust,
+                pkt: _quote.pkt,
               ),
             ),
-            Flexible(
-              child: TextFormField(
-                decoration: InputDecoration(labelText: 'art.'),
-                onSaved: (value) => _quote = Quote(
-                  source: _quote.source,
-                  art: value,
-                  par: _quote.par,
-                  ust: _quote.ust,
-                  pkt: _quote.pkt,
-                ),
+          ),
+          Flexible(
+            child: TextFormField(
+              decoration: InputDecoration(labelText: 'art.'),
+              onSaved: (value) => _quote = Quote(
+                source: _quote.source,
+                art: value,
+                par: _quote.par,
+                ust: _quote.ust,
+                pkt: _quote.pkt,
               ),
             ),
-            Flexible(
-              child: TextFormField(
-                decoration: InputDecoration(labelText: '\u00A7'),
-                onSaved: (value) => _quote = Quote(
-                  source: _quote.source,
-                  art: _quote.art,
-                  par: value,
-                  ust: _quote.ust,
-                  pkt: _quote.pkt,
-                ),
+          ),
+          Flexible(
+            child: TextFormField(
+              decoration: InputDecoration(labelText: '\u00A7'),
+              onSaved: (value) => _quote = Quote(
+                source: _quote.source,
+                art: _quote.art,
+                par: value,
+                ust: _quote.ust,
+                pkt: _quote.pkt,
               ),
             ),
-            Flexible(
-              child: TextFormField(
-                decoration: InputDecoration(labelText: 'ust.'),
-                onSaved: (value) => _quote = Quote(
-                  source: _quote.source,
-                  art: _quote.art,
-                  par: _quote.par,
-                  ust: value,
-                  pkt: _quote.pkt,
-                ),
+          ),
+          Flexible(
+            child: TextFormField(
+              decoration: InputDecoration(labelText: 'ust.'),
+              onSaved: (value) => _quote = Quote(
+                source: _quote.source,
+                art: _quote.art,
+                par: _quote.par,
+                ust: value,
+                pkt: _quote.pkt,
               ),
             ),
-            Flexible(
-              child: TextFormField(
-                decoration: InputDecoration(labelText: 'pkt'),
-                onSaved: (value) => _quote = Quote(
-                  source: _quote.source,
-                  art: _quote.art,
-                  par: _quote.par,
-                  ust: _quote.ust,
-                  pkt: value,
-                ),
+          ),
+          Flexible(
+            child: TextFormField(
+              decoration: InputDecoration(labelText: 'pkt'),
+              onSaved: (value) => _quote = Quote(
+                source: _quote.source,
+                art: _quote.art,
+                par: _quote.par,
+                ust: _quote.ust,
+                pkt: value,
               ),
             ),
-            ElevatedButton(onPressed: submitQuote, child: Text("Kopiuj"))
-          ],
-        ),
+          ),
+          ElevatedButton(onPressed: submitQuote, child: Text("Kopiuj")),
+        ],
       ),
     );
   }
 
   void submitQuote() {
     _formKey.currentState.save();
+    final quoteCubit = context.read<QuoteCubit>();
+    quoteCubit.setQuote(_quote.toString());
   }
 }
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:paste_law/cubit/quote_cubit.dart';
 import 'package:paste_law/screens/home_screen/components/home_form.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,8 +10,21 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Paste Law'),
       ),
-      body: Container(
-        child: HomeForm(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Flexible(child: HomeForm()),
+            BlocBuilder<QuoteCubit, QuoteState>(
+              builder: (context, state) {
+                if (state is QuoteCreated)
+                  return Text(state.quote);
+                else
+                  return Text('Wprowadz text');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
